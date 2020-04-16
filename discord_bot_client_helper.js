7431aa5fc362f481e5cc1b98487bdff8f0d3d4db
Solomon Lee <guoen4722@gmail.com>
2020-04-17 04:03:48 +0800

    new mechs

--- discord_bot_client_helper.js
+++ discord_bot_client_helper.js
@@ -301,8 +301,6 @@ class scheduler_helper {
                         spammer = discordUncompletedMembers.join('\n');
                     }
 
-                    var spamMessage = 'Unfortunately the below list of members have yet to fill RSVP!' + '\n' + spammer;
-                    msg.delete(this.delay);
                     msg.author.send(spamMessage);
                 }
 
@@ -310,8 +308,6 @@ class scheduler_helper {
             else {
                 var spammer = discordUncompletedMembers.join('\n');
 
-                var spamMessage = 'Unfortunately the below list of members have yet to fill RSVP!' + '\n' + spammer;
-                msg.delete(this.delay);
                 msg.author.send(spamMessage);
             }
         })
